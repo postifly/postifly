64c1030f076afdf812a354d66005a4fbf39ecbcd
vaqsi1990 <vaqsii23@gmail.com>
2026-03-31 14:56:19 +0400

    hero

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -22,11 +22,11 @@ export default function Hero() {
         sizes="100vw"
       />
       <div className="relative mx-auto grid min-h-[520px] max-w-screen-1xl grid-cols-1 items-start px-4 py-6 sm:px-6 md:min-h-[560px] md:py-8 lg:px-5">
-        <div className="relative z-10 max-w-2xl rounded-2xl p-6 sm:p-8 ">
-          <h1 className="text-balance text-[25px] md:text-[30px] text-center lg:text-left font-semibold leading-tight text-white ">
+        <div className="relative z-10 flex justify-center  flex-col max-w-2xl rounded-2xl p-6 sm:p-8 ">
+          <p className="text-balance text-[25px] md:text-[30px] font-semibold leading-tight text-white ">
             POSTIFLY
-          </h1>
-          <p className="mt-4 text-pretty text-[25px] md:text-[30px] leading-relaxed text-white ">
+          </p>
+          <p className="mt-4 text-pretty text-[25px] md:text-[30px]  leading-relaxed text-white ">
             {line1}
           </p>
           <p className=" text-pretty text-[25px] md:text-[30px] leading-relaxed text-white ">
@@ -34,7 +34,7 @@ export default function Hero() {
           </p>
           <Link
             href="#tariffs"
-            className="mt-5 inline-flex rounded-lg bg-[#3a5bff] px-6 py-3 text-base font-semibold text-white transition hover:opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/80 focus-visible:ring-offset-2 focus-visible:ring-offset-[#3a5bff]"
+            className="mt-5 w-[150px] rounded-xl inline-flex flex justify-center items-center  text-centerrounded-lg text-center bg-[#3a5bff] px-6 py-3 text-base font-semibold text-white transition hover:opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/80 focus-visible:ring-offset-2 focus-visible:ring-offset-[#3a5bff] mx-auto lg:mx-0"
           >
             {t("tariffsTitle")}
           </Link>
