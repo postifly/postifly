330777318a2500ea116658cd06257ec356437844
vaqsi1990 <vaqsii23@gmail.com>
2026-04-24 21:45:39 +0400

    translate

--- app/[locale]/support/users/new/page.tsx
+++ app/[locale]/support/users/new/page.tsx
@@ -1,16 +1,18 @@
 import SupportShell from '@/app/support/components/SupportShell';
 import AdminCreateUserForm from '@/app/admin/users/components/AdminCreateUserForm';
+import { getTranslations } from 'next-intl/server';
 
 export const dynamic = 'force-dynamic';
 
 type Props = { params: Promise<{ locale: string }> };
 
-export default function SupportUsersNewPageLocale(props: Props) {
+export default async function SupportUsersNewPageLocale(props: Props) {
   void props;
+  const t = await getTranslations('supportUsersNew');
   return (
     <SupportShell
-      title="ახალი მომხმარებელი"
-      description="შეავსეთ ფორმა ახალი მომხმარებლის ხელით რეგისტრაციისთვის."
+      title={t('title')}
+      description={t('description')}
     >
       <AdminCreateUserForm postUrl="/api/admin/users" successRedirect="/support/users" />
     </SupportShell>
